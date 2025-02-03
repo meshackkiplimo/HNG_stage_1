6f204b822d8e5e1422286ffc110ab31d72e887db
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-02-03 21:14:11 +0300

    error:deploymebt

--- index.ts
+++ index.ts
@@ -8,7 +8,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'Server is running', 
+    endpoints: ['/api/classify-number']
+  });
+});
 app.get("/api/classify-number", classifyNumber);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
